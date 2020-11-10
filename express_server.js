07763fadaccbfbdf1e1088f5fdd4b842015a9755
RAFH82 <hrechkar@gmail.com>
2020-11-10 03:10:22 +0000

    added bodyparser functionality

--- express_server.js
+++ express_server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
-app.set("view engine", "ejs");
-
+const bodyParser = require("body-parser");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com",
 };
+
+function generateRandomString() {
+  
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -16,6 +21,15 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+app.get("/urls_new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
