3bd53cc4958820eb268ea92babe459795ffc65c4
RAFH82 <hrechkar@gmail.com>
2020-11-10 03:39:35 +0000

    added random string generation function

--- express_server.js
+++ express_server.js
@@ -8,8 +8,79 @@ const urlDatabase = {
 };
 
 function generateRandomString() {
-  
-};
+  const sourceArr = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const indexOne = Math.floor(Math.random() * 63);
+  const indexTwo = Math.floor(Math.random() * 63);
+  const indexThree = Math.floor(Math.random() * 63);
+  const indexFour = Math.floor(Math.random() * 63);
+  const indexFive = Math.floor(Math.random() * 63);
+  const indexSix = Math.floor(Math.random() * 63);
+  let outputStr = `${sourceArr[indexOne]}${sourceArr[indexTwo]}${sourceArr[indexFour]}${sourceArr[indexFive]}${sourceArr[indexSix]}`;
+  return outputStr;
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
