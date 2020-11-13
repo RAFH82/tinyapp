ffc971b6ccaf2c4b8a0e859befcfdf8f4ae7e8e8
RAFH82 <hrechkar@gmail.com>
2020-11-13 03:52:38 +0000

    removed old database and user defaults. both are now empty at start of server

--- data.js
+++ data.js
@@ -1,26 +1,6 @@
-const urlDatabase = {
-  "b2xVn2": {
-    longURL: "http://www.lighthouselabs.ca",
-    userId: "T123YZ",
-  },
-  "9sm5xk": {
-    longURL: "http://www.google.com",
-    userId: "123XYZ",
-  } 
-};
+const urlDatabase = {};
 
-const users = { 
-  "T123YZ": {
-    id: "T123YZ", 
-    email: "t123yz@example.com", 
-    password: "purple"
-  },
- "123XYZ": {
-    id: "123XYZ", 
-    email: "123xyz@example.com", 
-    password: "dishwasher"
-  },
-}
+const users = {};
 
 module.exports = { urlDatabase, users };
 
